29b344cfd2a5d347d1372f6f34be42a8e7f72c87
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2024-03-19 23:44:22 +0100

    artistic choice
    it just looks better with a lower gradient

--- src/shaders/waterMaterial/vertex.glsl
+++ src/shaders/waterMaterial/vertex.glsl
@@ -21,10 +21,10 @@ const float scalingFactor = 1e-5;
 
 vec3 sampleHeightAndGradient(vec2 point) {
     float height = texture(heightMap, point).r;
-    vec2 gradient = texture(gradientMap, point).rg;
+    vec2 gradient = texture(gradientMap, point).rg * 0.1; // the 0.1 here is just for artistic reasons
     vec3 heightAndGradient = vec3(height, gradient);
 
-    return heightAndGradient * scalingFactor * 0.05;
+    return heightAndGradient * scalingFactor * 0.5;
 }
 
 void main() {
@@ -37,6 +37,12 @@ void main() {
     vec3 heightAndGradient = sampleHeightAndGradient(uv);
     waterPosition.y += heightAndGradient.x;
     vec3 normal = normalize(vec3(-heightAndGradient.y, 1.0, -heightAndGradient.z));
+
+    // normal using central difference
+    /*float epsilon = 0.001;
+    vec3 tangent1 = normalize(vec3(uv.x + epsilon, sampleHeightAndGradient(uv + vec2(epsilon, 0.0)).x, uv.y) - vec3(uv.x - epsilon, sampleHeightAndGradient(uv - vec2(epsilon, 0.0)).x, uv.y));
+    vec3 tangent2 = normalize(vec3(uv.x, sampleHeightAndGradient(uv + vec2(0.0, epsilon)).x, uv.y + epsilon) - vec3(uv.x, sampleHeightAndGradient(uv - vec2(0.0, epsilon)).x, uv.y - epsilon));
+    vec3 normal = -normalize(cross(tangent1, tangent2));*/
 
     vPosition = waterPosition;
     vPositionW = vec3(world * vec4(waterPosition, 1.0));
